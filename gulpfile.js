7a819853a28f1e7a1378dbcc41ee92f882dfd1e1
Chris Ruppel <chris.ruppel@gmail.com>
2015-08-13 12:26:27 +0200

    :zap: inline all CSS

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@ gulp.task('sass', 'Compiles Sass using libsass.', function () {
     .pipe(rename('main.min.css'))
     .pipe(gulp.dest('css'))
     .pipe(gulp.dest('_site/css'))
-    // .pipe(gulp.dest('_includes')) // uncomment to create inline CSS file
+    .pipe(gulp.dest('_includes'))
     .pipe(reload({stream: true}));
 });
 
