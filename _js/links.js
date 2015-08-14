6b94faa266f2261ffa3ac5043c6e5804d88e7b92
Chris Ruppel <chris.ruppel@gmail.com>
2015-08-14 15:39:40 +0200

    :flushed: only process links when they're present

--- _js/links.js
+++ _js/links.js
@@ -1,10 +1,15 @@
 var trip = document.querySelector('.trip');
 var paragraphs = document.querySelectorAll('.trip p');
 var links = document.querySelector('section.links');
-var copy = links.cloneNode(true);
 
-// Remove original links from DOM
-links.parentNode.removeChild(links);
+// If the links were found
+if (links !== null) {
+  // Make a copy of the links
+  var copy = links.cloneNode(true);
 
-// Re-insert links shortly before the end of the story
-trip.insertBefore(copy, paragraphs[paragraphs.length - 3]);
+  // Remove original links from DOM
+  links.parentNode.removeChild(links);
+
+  // Re-insert links shortly before the end of the story
+  trip.insertBefore(copy, paragraphs[paragraphs.length - 3]);
+}
