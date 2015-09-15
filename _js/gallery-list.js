188f69e3739a38b3dc050bc081ae3f332e710b80
Chris Ruppel <chris.ruppel@gmail.com>
2015-09-15 12:19:21 +0200

    :zap: lazy-load gallery list

---
+++ _js/gallery-list.js
@@ -0,0 +1,41 @@
+/**
+ * Gallery list: load more
+ */
+if ('content' in document.createElement('template')) {
+  // All possible content is stored in this <template>
+  var template = document.querySelector('#gallery-list');
+
+  // If <template> is not supported, the browser will just show the date.
+  // When everything is working, it can load more using the contents.
+  if (template !== null) {
+    var content = template.content.querySelectorAll('.teaser');
+    var trip_list = document.querySelector('#trip-list');
+
+    // Turn trips into real array.
+    var trips = [];
+    for (var i = content.length >>> 0; i--;) {
+      trips[i] = content[i];
+    }
+
+    // Create a 'load more' link
+    var loadmore = document.createElement('a');
+    loadmore.classList.add('btn', 'load-more');
+    loadmore.textContent = 'Load More';
+    document.querySelector('.go').appendChild(loadmore);
+
+    // Click handler for 'load more' link
+    loadmore.addEventListener('click', function loadMore () {
+      var next_trip = trips.shift();
+      // If there's content, load it.
+      if (next_trip) {
+        trip_list.appendChild(next_trip);
+      }
+      // If not, disable the button.
+      else {
+        loadmore.textContent = 'You reached the end!';
+        loadmore.classList.add('disabled');
+        loadmore.removeEventListener('click', loadMore);
+      }
+    });
+  }
+}
---
+++ _js/trips.js
@@ -0,0 +1,20 @@
+/**
+ * Fetch trip data
+ */
+// var request = new XMLHttpRequest();
+// request.open('GET', '/travel.json', true);
+
+// request.onload = function() {
+//   if (this.status >= 200 && this.status < 400) {
+//     // get crunchin'
+//   } else {
+//     // boo
+//     console.error('😲 Couldn\'t fetch gallery data.');
+//   }
+// };
+
+// request.onerror = function() {
+//   // There was a connection error of some sort
+// };
+
+// request.send();
