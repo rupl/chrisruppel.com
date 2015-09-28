b183b4326296add52eac25e49b819a0b3cf14fca
Chris Ruppel <chris.ruppel@gmail.com>
2015-09-28 20:07:53 +0200

    :construction: hackety hack

--- gulpfile.js
+++ gulpfile.js
@@ -127,6 +127,7 @@ gulp.task('default', false, ['help']);
 // -----------------------------------------------------------------------------
 gulp.task('build-deploy', 'Do a complete build to prep for deploy.', function(cb) {
   return sequence(
+    'jekyll-deploy', // for whatever reason, I find this needing to be run before and after
     ['sass', 'js', 'imagemin'],
     'jekyll-deploy',
     cb
