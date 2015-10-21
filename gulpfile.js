4e6167374b00c98ec5967b87f716e992e7645ce2
Chris Ruppel <chris.ruppel@gmail.com>
2015-10-21 11:39:16 +0700

    :construction: ignore unnamed GOPRO images

--- gulpfile.js
+++ gulpfile.js
@@ -112,7 +112,7 @@ gulp.task('js', 'Lint, bundle, minify JS', function() {
 // Minify images
 // -----------------------------------------------------------------------------
 gulp.task('imagemin', 'Compress images.', function() {
-  return gulp.src(['_img/**/*', '!_img/**/{IMG_,DSC_,DSCF}*'])
+  return gulp.src(['_img/**/*', '!_img/**/{IMG_,DSC_,DSCF,GOPR}*'])
     .pipe(changed('img'))
     .pipe(imagemin({
       progressive: true,
