84f99e19987761690e25a4789b09b2402f439499
Chris Ruppel <chris.ruppel@gmail.com>
2015-11-12 15:13:23 +0700

    :flushed: missing semi

--- _js/_qualify.js
+++ _js/_qualify.js
@@ -2,7 +2,7 @@
 var oswaldLight = new FontFaceObserver('Oswald', {weight: 300});
 var oswaldRegular = new FontFaceObserver('Oswald', {weight: 400});
 
-var observer = window.Promise.all([oswaldLight.check(), oswaldRegular.check()])
+var observer = window.Promise.all([oswaldLight.check(), oswaldRegular.check()]);
 observer.then(function () {
   document.documentElement.classList.remove('wf-loading');
   document.documentElement.classList.add('wf-oswald');
