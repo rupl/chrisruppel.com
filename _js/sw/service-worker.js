80fb651b89cc0c1157860c101517a0a2ac59fab3
Chris Ruppel <chris.ruppel@gmail.com>
2015-12-07 14:00:41 +0700

    :construction: decent support for offline content

--- _js/sw/service-worker.js
+++ _js/sw/service-worker.js
@@ -19,7 +19,9 @@ var SW = {
   ]
 };
 
+//
 // Installation
+//
 self.addEventListener('install', function installer (event) {
   event.waitUntil(
     caches
@@ -40,16 +42,57 @@ self.addEventListener('install', function installer (event) {
   );
 });
 
-// Activation
+//
+// Activation. First-load and also when a new version of SW is detected.
+//
 self.addEventListener('activate', function(event) {
   console.info("Service Worker: activated");
 });
 
+//
 // Intercept requests
+//
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      return response || fetch(event.request);
-    })
-  );
+  // Build a hostname-free version
+  var reqLocation = getLocation(event.request.url);
+  var reqPath = reqLocation.pathname;
+
+  // Catch offline requests to useless URLs and serve offline page instead.
+  if (
+    event.request.method === 'GET' &&
+    event.request.headers.get('accept').includes('text/html') &&
+    SW.offline_assets.indexOf(reqPath) === -1
+  ) {
+    event.respondWith(
+      fetch(event.request).catch(function(error) {
+        return caches.open(SW.cache_version).then(function(cache) {
+          console.info('Serving offline page.');
+          return cache.match('/offline/');
+        });
+      })
+    );
+  } else {
+    // Fallback cache for generic assets.
+    event.respondWith(
+      caches.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      })
+    );
+  }
+
 });
+
+// @see http://stackoverflow.com/a/21553982/175551
+function getLocation(href) {
+  var match = href.match(/^(https?\:)\/\/(([^:\/?#]*)(?:\:([0-9]+))?)(\/[^?#]*)(\?[^#]*|)(#.*|)$/);
+
+  return match && {
+    protocol: match[1],
+    host: match[2],
+    hostname: match[3],
+    port: match[4],
+    pathname: match[5],
+    search: match[6],
+    hash: match[7]
+  }
+}
