3818449681cf4b458c7776312b211ce803301caa
Chris Ruppel <chris.ruppel@gmail.com>
2015-12-10 21:39:05 +0700

    :closed_lock_with_key: tighten security with various HTTP headers

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
 // Node/npm deps
 var express = require('express');
 var port = process.env.PORT || 5000;
+var helmet = require('helmet');
+var csp = require('helmet-csp');
 var enforce = require('express-sslify');
 var compression = require('compression');
 
 // Initialize app
 var app = express();
 
-// Force HTTPS on Heroku
+// HTTP headers: hidePoweredBy, hsts, ieNoOpen, noSniff, frameguard, xssFilter
+app.use(helmet());
+
+// HTTP header: contentSecurityPolicy
+app.use(csp({
+  // Policy for chrisruppel.com
+  defaultSrc: ["'self'"],
+  scriptSrc: ["'unsafe-inline'", "'*.google-analytics.com'", "'*.disqus.com'", "'*.disquscdn.com'", "'*.mapbox.com'"],
+  styleSrc: ["'unsafe-inline'", "'*.disqus.com'", "'*.disquscdn.com'", "'*.mapbox.com'"],
+  imgSrc: ["'*.google-analytics.com'", "'*.mapbox.com'", 'data:'],
+  fontSrc: [],
+  objectSrc: ["'youtube.com'"],
+  mediaSrc: ["'youtube.com'"],
+  frameSrc: [],
+  connectSrc: ["'none'"],
+  sandbox: ['allow-scripts'],
+
+  // Set to true if you only want browsers to report errors, not block them
+  reportOnly: false,
+
+  // Set to true if you want to blindly set all headers: Content-Security-Policy,
+  // X-WebKit-CSP, and X-Content-Security-Policy.
+  setAllHeaders: false,
+
+  // Set to true if you want to disable CSP on Android.
+  disableAndroid: false,
+
+  // Set to true if you want to force buggy CSP in Safari 5.1 and below.
+  safari5: false
+}));
+
+// Redirect HTTP to HTTPS on Heroku
 app.use(enforce.HTTPS({trustProtoHeader: true}));
 
 // Compress all responses
