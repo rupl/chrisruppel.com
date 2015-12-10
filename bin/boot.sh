50bf79a51bfc2debb3d2e8116cd21d0702f24c1a
Chris Ruppel <chris.ruppel@gmail.com>
2015-12-10 17:44:25 +0700

    :construction: debugging and comments for boot.sh

--- bin/boot.sh
+++ bin/boot.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# debugging
+pwd
+
+# For whatever reason when the slug compiles, it doesn't seem to use the latest
+# version of node so we have to rebuild the package to get Sass working
 npm rebuild node-sass
+
+# Image resizing spun off into background to avoid blocking web process
 node node_modules/gulp/bin/gulp image-resize &
+
+# Build jekyll and start Express
 node node_modules/gulp/bin/gulp build-deploy
 node index.js
