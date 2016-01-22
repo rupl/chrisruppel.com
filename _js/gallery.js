0bee38396ca0f07201f48fb96bb2ef2ab49e1cfd
Chris Ruppel <chris.ruppel@gmail.com>
2016-01-22 23:32:56 +0800

    :hammer: better comments for gallery functions

--- _js/gallery.js
+++ _js/gallery.js
@@ -5,12 +5,15 @@
 var gallery = document.querySelectorAll('.gallery .photo');
 var forEach = Array.prototype.forEach;
 
-// Did we find images?
+// Did we find images? If yes, process them immediately.
 if (gallery.length > 0) {
   processGalleryPhotos(gallery)
 }
 
 // Process the gallery photos
+//
+// Uses the result of the responsive images process to display properly-selected
+// background-images in our gallery slots.
 function processGalleryPhotos(photos) {
   forEach.call(photos, function (photo) {
     // debug
@@ -38,6 +41,7 @@ function processGalleryPhotos(photos) {
   });
 }
 
+// Helper function to close any captions that are currently open.
 function removeCaptions() {
   var captions = document.querySelectorAll('.photo--caption');
   forEach.call(captions, function (photo) {
