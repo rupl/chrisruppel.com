6e3e505670c708c586570e7fae6b3720bc8e71b8
Chris Ruppel <chris.ruppel@gmail.com>
2016-01-26 12:21:31 +0800

    :zap: add Stale While Revalidate to SW

--- _js/sw/service-worker.js
+++ _js/sw/service-worker.js
@@ -8,12 +8,13 @@ importScripts('/js/cache-polyfill.js');
 
 // Config
 var SW = {
-  cache_version: 'main_v1.2.0',
+  cache_version: 'main_v1.3.0',
   offline_assets: [
     '/',
+    '/offline/',
     '/work/',
     '/about/',
-    '/offline/',
+    '/about/site/',
     '/travel/list/',
     '/css/main.min.css',
     '/css/fonts.min.css',
@@ -78,34 +79,91 @@ self.addEventListener('activate', function(event) {
 // Intercept requests
 //
 self.addEventListener('fetch', function(event) {
-  // Build a hostname-free version
+  // Build a hostname-free version of request path.
   var reqLocation = getLocation(event.request.url);
+  var reqHost = reqLocation.hostname;
   var reqPath = reqLocation.pathname;
 
-  // Catch offline requests to useless URLs and serve offline page instead.
+  // Consolidate some conditions for re-use.
+  var requestIsHTML = event.request.headers.get('accept').includes('text/html');
+  var requestIsAsset = /^(\/css\/|\/js\/)/.test(reqPath);
+
+  // Offline HTML
+  //
+  // Intercept offline requests to uncached URLs and serve offline page instead.
   if (
+    requestIsHTML &&
     event.request.method === 'GET' &&
-    event.request.headers.get('accept').includes('text/html') &&
     SW.offline_assets.indexOf(reqPath) === -1
   ) {
     event.respondWith(
       fetch(event.request).catch(function(error) {
         return caches.open(SW.cache_version).then(function(cache) {
-          console.info('Serving offline page.');
+          console.info('Fetch listener served offline page.');
           return cache.match('/offline/');
         });
       })
     );
-  } else {
-    // Fallback cache for generic assets.
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
+  }
+
+  // Stale While Revalidate
+  //
+  // SW will respond with cache if there's a hit, but look for a new version
+  // in the background so that the next page load will be fresh.
+  //
+  // @see http://12devsofxmas.co.uk/2016/01/day-9-service-worker-santas-little-performance-helper/
+  else if (requestIsHTML || requestIsAsset) {
+    event.respondWith(returnFromCacheOrFetch(event.request));
+  }
+
+  // Uncaught
+  //
+  // This request fell through all our conditions and is being ignored by SW.
+  else {
+    console.info('Fetch listener ignored ' + reqPath);
   }
 
 });
+
+// Helper function to handle Fetch listener.
+function returnFromCacheOrFetch(request) {
+  // Build a hostname-free version of request path.
+  var reqLocation = getLocation(request.url);
+  var reqPath = reqLocation.pathname;
+
+  var cachePromise = caches.open(SW.cache_version);
+  var matchPromise = cachePromise.then(function(cache) {
+    return cache.match(request);
+  });
+
+  return Promise.all([cachePromise, matchPromise]).then(function(promiseResults) {
+    // When ES2015 isn't behind a flag anymore, move these two vars to an array
+    // in the function signature to destructure the results of the Promise.
+    var cache = promiseResults[0];
+    var cacheResponse = promiseResults[1];
+
+    // Kick off the update request in the background.
+    var fetchPromise = fetch(request).then(function(fetchResponse) {
+      // If the resource is unchanged, skip the caching process.
+      if (fetchResponse.status !== 304) {
+        // Cache the updated file and then return the response
+        cache.put(request, fetchResponse.clone());
+        console.info('Fetch listener updated ' + reqPath);
+      }
+      else {
+        console.info('Fetch listener skipped ' + reqPath);
+      }
+
+      // Return response regardless of caching outcome.
+      return fetchResponse;
+    });
+
+    // Return the cached response if we have it, otherwise wait for the
+    // actual response to come back.
+    return cacheResponse || fetchPromise;
+  });
+}
+
 
 // @see http://stackoverflow.com/a/21553982/175551
 function getLocation(href) {
@@ -119,5 +177,5 @@ function getLocation(href) {
     pathname: match[5],
     search: match[6],
     hash: match[7]
-  }
+  };
 }
