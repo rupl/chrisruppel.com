65f5346c28efcffbdd83c9a114448b0b7b17fc38
Chris Ruppel <chris.ruppel@gmail.com>
2016-03-08 09:38:54 +0630

    :bug: fix regex error in cleanup script

--- bin/clean-imgs.sh
+++ bin/clean-imgs.sh
@@ -6,5 +6,5 @@ declare -a arr=("" "\@640" "\@320")
 ## now loop through the above array
 for i in "${arr[@]}"
 do
-  git s | grep _img | sed "s/_img\/travel/_site\/img\/travel$i/g" | sed 's/[?ADM ]//g' | xargs -n1 rm
+  git s | grep _img | sed "s/_img\/travel/_site\/img\/travel$i/g" | sed 's/[?ADM ]//' | xargs -n1 rm
 done
