eda811236c1634bacc66aaabf87a3c6b2579d229
Chris Ruppel <chris.ruppel@gmail.com>
2016-03-18 16:58:24 +0700

    :cop: clean up trip file before adding more code

--- _js/trip.js
+++ _js/trip.js
@@ -2,14 +2,19 @@
 // Trip entry enhancements
 //
 
-var entry_uid = document.querySelector('#uid');
+//
+// Permalink focus/blur
+//
+(function () {
+  var entry_uid = document.querySelector('#uid');
 
-if (entry_uid) {
-  // Highlight permalink when clicked/tapped
-  document.querySelector('#uid').addEventListener('click', copyUid);
-  document.querySelector('label[for=uid]').addEventListener('click', copyUid);
-}
+  if (entry_uid) {
+    // Highlight permalink when clicked/tapped
+    document.querySelector('#uid').addEventListener('click', copyUid);
+    document.querySelector('label[for=uid]').addEventListener('click', copyUid);
+  }
 
-function copyUid() {
-  document.querySelector('#uid').select();
-}
+  function copyUid() {
+    document.querySelector('#uid').select();
+  }
+});
