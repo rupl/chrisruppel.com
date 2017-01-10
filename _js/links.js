5cc920f143d19af610798b59ccfa7d5edb3412db
Chris Ruppel <chris.ruppel@gmail.com>
2017-01-10 12:27:46 +0100

    :bug: fix links code

--- _js/links.js
+++ _js/links.js
@@ -1,17 +1,19 @@
-var trip = document.querySelector('.story');
-var paragraphs = document.querySelectorAll('.story > p');
-var links = document.querySelector('.trip .links');
+(function () {
+  var entry = document.querySelector('.e-content');
+  var paragraphs = document.querySelectorAll('.e-content > p');
+  var links = document.querySelector('.h-entry--main .links');
 
-// If the links were found
-if (links !== null) {
-  // Make a copy of the links
-  var copy = links.cloneNode(true);
+  // If the links were found
+  if (links !== null) {
+    // Make a copy of the links
+    var copy = links.cloneNode(true);
 
-  // Remove original links from DOM
-  links.parentNode.removeChild(links);
+    // Remove original links from DOM
+    links.parentNode.removeChild(links);
 
-  // Re-insert links shortly before the end of the story. If the story is very
-  // short, insert them after first paragraph.
-  var position = (paragraphs.length > 3) ? paragraphs[paragraphs.length - 2] : paragraphs[1];
-  trip.insertBefore(copy, position);
-}
+    // Re-insert links shortly before the end of the story. If the story is very
+    // short, insert them after first paragraph.
+    var position = (paragraphs.length > 3) ? paragraphs[paragraphs.length - 2] : paragraphs[1];
+    entry.insertBefore(copy, position);
+  }
+})();
