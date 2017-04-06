10c83bd24fd77de387c200b86b1bee29aa34fd5a
Chris Ruppel <chris.ruppel@gmail.com>
2017-04-06 14:45:09 +0200

    :bug: ensure Calendar is up to date. Fixes #64.
    The Calendar can be far out of date if someone with SW
    revisits after a long period of time. Some JS tries to
    fetch the latest calendar and if it fails for any reason
    it warns the user that it's old.

--- _js/work.js
+++ _js/work.js
@@ -3,4 +3,22 @@
   $$('.available').on('click', function () {
     $('.btn--mail').click();
   });
+
+  // For users with SW, fetch latest calendar if available.
+  // If user does not have SW, then they won't see cached HTML in the first place.
+  if (Modernizr.serviceworker) {
+    fetch('/work/calendar/').then(function (data) {
+      return data.text();
+    }).then(function (text) {
+      var calendar = $('#calendar');
+      calendar.innerHTML = text;
+      console.info('Updated calendar successfully.');
+    }).catch(function (error) {
+      console.error(error);
+      var warning = document.createElement('p');
+      warning.classList.add('warning');
+      warning.innerHTML = '<em><strong>Note:</strong></em> The latest calendar could not be fetched.';
+      $('#calendar').appendChild(warning);
+    });
+  }
 })();
