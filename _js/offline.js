2e994afcb6e01a5e4f2586b46c7bf697ddef16ea
Chris Ruppel <chris.ruppel@gmail.com>
2017-05-01 13:44:44 +0200

    :nut_and_bolt: reduce travel list to cached entries when offline
    Fixes #60.

--- _js/offline.js
+++ _js/offline.js
@@ -7,15 +7,20 @@
       // Define these just once instead of in the loop.
       var offlineContentList = $('#offline');
       var offlineContentEntry = $('#offline-content');
+      var travelList = $('.trip-list');
+      var travelContentEntries = $('.teaser--title');
 
       return Promise.all(
         // Loop through Cache entries.
         cacheNames.map(function(cacheName) {
           // Check if any have the offline article naming convention.
           if (cacheName.indexOf(OFFLINE_ARTICLE_PREFIX) !== -1) {
+            // Extract the URL from cache name
+            var cachedURL = cacheName.split('--')[1];
+
             // The cache name indicates that it was saved by the user.
             var cacheEntry = document.createElement('li');
-            cacheEntry.innerHTML = '<a href="' + cacheName.split('--')[1] + '">' + cacheName.split('/')[2].split('-').join(' ') + '</a>';
+            cacheEntry.innerHTML = '<a href="' + cachedURL + '">' + cacheName.split('/')[2].split('-').join(' ') + '</a>';
 
             // Append to DOM.
             if (!!offlineContentEntry) {
@@ -26,6 +31,16 @@
             // Set the offline box to display since we found something
             if (!!offlineContentList) {
               offlineContentList.classList.add('is-active');
+            }
+
+            // Set the Travel list to offline mode when Offline, but tag the
+            // cached entries so they stay visible.
+            if (!navigator.onLine && !!travelContentEntries) {
+              travelList.classList.add('is-offline');
+              var cachedEntry = $('.teaser--title[data-url="' + cachedURL + '"');
+              if (!!cachedEntry) {
+                cachedEntry.classList.add('is-cached');
+              }
             }
           }
         })
