e1b92e1b1f2cfae69cf284ed4ed32f0351a54ea4
Chris Ruppel <chris.ruppel@gmail.com>
2017-07-25 15:19:01 +0200

    :cop: remove unused commented code from SW

--- _js/sw/service-worker.js
+++ _js/sw/service-worker.js
@@ -9,7 +9,7 @@ importScripts('/js/cache-polyfill.js');
 // Config
 var OFFLINE_ARTICLE_PREFIX = 'chrisruppel-offline--';
 var SW = {
-  cache_version: 'main_v1.8.2',
+  cache_version: 'main_v1.8.3',
   offline_assets: [
     '/',
     '/offline/',
@@ -102,7 +102,6 @@ self.addEventListener('activate', function(event) {
 self.addEventListener('fetch', function(event) {
   // Build a hostname-free version of request path.
   var reqLocation = getLocation(event.request.url);
-  // var reqHost = reqLocation.hostname;
   var reqPath = reqLocation.pathname;
 
   // Consolidate some conditions for re-use.
