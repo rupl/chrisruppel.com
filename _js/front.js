cd29213b484ada75f8e854f290cc482681460891
Chris Ruppel <chris.ruppel@gmail.com>
2018-04-13 14:59:54 +0200

    :nut_and_bolt: add uncached path for latest blog posts
    Fixes #82

--- _js/front.js
+++ _js/front.js
@@ -4,27 +4,22 @@
   // TODO: instead of using data-autoupdate="true" populate the attribute with
   //       the path that should be requested, then the data-attribute can serve
   //       as both a flag and the argument for a generic function.
-  var calendar = $('#calendar[data-autoupdate=true]');
+  var blogList = $('#blog-list[data-autoupdate=true]');
 
-  // Load the contact form when an open slot is clicked.
-  $$('.available').on('click', function () {
-    $('.btn--mail').click();
-  });
-
-  // For users with SW, fetch latest calendar if available.
+  // For users with SW, fetch latest blog posts if available.
   // If user does not have SW, then they won't see cached HTML in the first place.
-  if (Modernizr.serviceworker && !!calendar) {
+  if (Modernizr.serviceworker && !!blogList) {
     // TODO: use value of data-autoupdate in this `fetch()`
-    fetch('/work/calendar/').then(function (data) {
+    fetch('/blog/latest/').then(function (data) {
       return data.text();
     }).then(function (text) {
-      calendar.innerHTML = text;
-      console.info('Updated calendar successfully.');
+      blogList.innerHTML = text;
+      console.info('Updated latest blog posts successfully.');
     }).catch(function (error) {
       var warning = document.createElement('aside');
       warning.classList.add('warning');
-      warning.innerHTML = '<p><em><strong>Note:</strong></em> This calendar might be slightly out of date.</p>';
-      calendar.appendChild(warning);
+      warning.innerHTML = '<p><em><strong>Note:</strong></em> This list might be slightly out of date.</p>';
+      blogList.appendChild(warning);
       console.error(error);
     });
   }
