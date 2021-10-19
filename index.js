6de8e031b472121d2b8694c86fa9b6ef083b3221
Chris Ruppel <chris.ruppel@gmail.com>
2021-10-19 15:07:46 +0200

    :chart_with_upwards_trend: delete Matomo-related entries from CSP

--- index.js
+++ index.js
@@ -45,10 +45,10 @@ app.use(helmet.contentSecurityPolicy({
   // Policy for chrisruppel.com
   directives:  {
     defaultSrc: ["'self'"],
-    scriptSrc:  ["'self'", "*.mapbox.com", "codepen.io", "*.codepen.io", "analytics.chrisruppel.com", "'unsafe-inline'", "'unsafe-eval'"],
+    scriptSrc:  ["'self'", "*.mapbox.com", "codepen.io", "*.codepen.io", "'unsafe-inline'", "'unsafe-eval'"],
     workerSrc:  ["'self'", "*.mapbox.com", "blob:"],
     styleSrc:   ["'self'", "*.mapbox.com", "fonts.googleapis.com", "'unsafe-inline'"],
-    imgSrc:     ["'self'", "*.mapbox.com", "assets.chrisruppel.com", "analytics.chrisruppel.com", "data:", "blob:"],
+    imgSrc:     ["'self'", "*.mapbox.com", "assets.chrisruppel.com", "data:", "blob:"],
     connectSrc: ["'self'", "*.mapbox.com", "assets.chrisruppel.com"],
     fontSrc:    ["'self'", "fonts.gstatic.com"],
     objectSrc:  ["youtube.com", "youtube-nocookie.com"],
