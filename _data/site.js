53ec9ab730392f10aabb27d49fb9d1da922aab1e
Chris Ruppel <chris.ruppel@gmail.com>
2022-09-16 09:17:46 +0200

    :closed_lock_with_key: remove hardcoded env vars
    I cycled them too. Sorry if you wanted to use them.

--- _data/site.js
+++ _data/site.js
@@ -8,9 +8,9 @@ module.exports = function() {
     'img-host': 'https://assets.chrisruppel.com',
     'license-url': 'https://github.com/rupl/chrisruppel.com/blob/master/LICENSE.md',
     'feedback-url': 'https://github.com/rupl/chrisruppel.com/issues/new?title=[Visitor+Feedback]+Your+title+here&body=Your+feedback+goes+here.+Thanks+for+taking+the+time+to+help+improve+my+website.&labels=Feedback',
-    'mapboxToken': 'pk.eyJ1IjoicnVwbCIsImEiOiI3NDc5MWQ4ZWNjMTg1ZmZlYWQ5ZjMwMWVkNjI2MTJlZCJ9.Szn0ysZDc6AjmhKnxf8PkA',
+    'mapboxToken': process.env.MAPBOX_TOKEN,
     'webmention-url': 'https://webmention.io/chrisruppel.com/webmention',
-    'webmention-token': 'iAxTpcBW1PpOtCOqHt2sBw',
+    'webmention-token': process.env.WEBMENTION_IO_TOKEN,
     'array': [],
   };
 };
